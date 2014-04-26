c92e2efc999300bc51bca78e86941e7055b1ad1a
Ted Meyer <tmathmeyer@gmail.com>
2014-04-26 19:06:46 -0400

    missing the talent and cost data, will look into before merge

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/HomeController.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/HomeController.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -49,6 +51,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) throws JsonGenerationException, JsonMappingException, IOException, ParseException
 	{
 		
+		Logger logger = LoggerFactory.getLogger(this.getClass());
 		
 		CachedStatsService css = CachedStatsService.getInstance(statsService);
 		CachedPeerService cps = CachedPeerService.getInstance(peersService);
@@ -61,9 +64,13 @@ public class HomeController {
 		State massEconomy = css.query("getStateBinData", "MA", 29);
 		PeerStates peers = cps.query("getPeers");
 		
+		logger.info("national datasize: {}", massNational.getParams().size());
+		logger.info("talent datasize: {}",massTalent.getParams().size());
+		logger.info("cost datasize: {}",massCost.getParams().size());
+		logger.info("economy datasize: {}",massEconomy.getParams().size());
+		
 		
 		// TODO un-hardcode these bin ids
-		// TODO cache this. a req to the db every time is why we keep going down
 		model.addAttribute("jv_stats_national", om.writeValueAsString(massNational.getParams()));
 		model.addAttribute("jv_stats_talent", om.writeValueAsString(massTalent.getParams()));
 		model.addAttribute("jv_stats_cost", om.writeValueAsString(massCost.getParams()));
--- MHTCDashboard/src/main/java/edu/wpi/mhtc/service/PeersServiceJDBC.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/service/PeersServiceJDBC.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
