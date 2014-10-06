a9655a8c866807ecdcc511bfc62cff28fe2a6984
cakuhlman <cakuhlman@130.215.28.158>
2014-10-06 17:38:54 -0400

    added rudimentary YearClearner

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/pipeline/cleaner/YearCleaner.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/pipeline/cleaner/YearCleaner.java
@@ -2,9 +2,21 @@ package edu.wpi.mhtc.dashboard.pipeline.cleaner;
 
 public class YearCleaner implements ICleaner {
 
+//	TODO: One file we found had 20121 20131 for years - need to remove extraneous digits
+	
+	
 	@Override
 	public String clean(String val) throws Exception {
-		return val;
+		
+		int year = Integer.parseInt(val);
+		if(1900 <= year && year <= 2015) return String.valueOf((int) year);
+		else throw new Exception("Not a valid year");
+	}
+	
+	public String clean(double val) throws Exception{
+		if(1900 <= val && val <= 2015) return String.valueOf((int) val);
+		
+		else throw new Exception("Not a valid year");
 	}
 
 }
