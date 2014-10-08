2987b3fb3052703faddc2473daf0014274f07f1c
hlong290494 <lhnguyenduc@wpi.edu>
2014-10-07 22:29:41 -0400

    Data.gov wrapper with smart downloader

---
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/pipeline/wrappers/DataGovDownload.java
@@ -0,0 +1,41 @@
+package edu.wpi.mhtc.dashboard.pipeline.wrappers;
+
+// Download JSON data from DataGov
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+
+
+public class DataGovDownload {
+	private String resource_id;
+	private String base_url = "https://inventory.data.gov/api/action/";
+	private URLDownload downloader;
+	
+	public DataGovDownload() {
+		downloader = new URLDownload();
+	}
+	
+	public void QueryDownload(String sql, String filename) {
+		String download_url = base_url + "datastore_search_sql?sql=" + sql;
+		URLDownload downloader = new URLDownload();
+		downloader.HTTPDownload(download_url, filename);
+	}
+	
+	// Smart file downloader, it goes into the government data API site,
+	// looks for the CSV URL of the download button, grabs and download that CSV file
+	// Tested for : https://inventory.data.gov/dataset/032e19b4-5a90-41dc-83ff-6e4cd234f565/resource/38625c3d-5388-4c16-a30f-d105432553a4
+	public void smartDownload(String url, String filename) throws IOException {
+		try {
+			org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
+			String downloadLink = doc.select("a.btn.btn-primary.resource-url-analytics.resource-type-None").first().attr("href");
+			downloader.HTTPDownload(downloadLink, filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
