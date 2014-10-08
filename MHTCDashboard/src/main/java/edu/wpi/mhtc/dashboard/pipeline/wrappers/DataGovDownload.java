eb3d8243fb099089649a544831ab666bd05be562
hlong290494 <lhnguyenduc@wpi.edu>
2014-10-08 12:54:29 -0400

    Intelligent login system for data.gov -  Can be used for other sites

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/pipeline/wrappers/DataGovDownload.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/pipeline/wrappers/DataGovDownload.java
@@ -3,7 +3,10 @@ package edu.wpi.mhtc.dashboard.pipeline.wrappers;
 // Download JSON data from DataGov
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.Map;
 
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -13,8 +16,10 @@ import org.w3c.dom.Document;
 public class DataGovDownload {
 	private String base_url = "https://inventory.data.gov/api/action/";
 	private URLDownload downloader;
+	private Map<String, String> cookies;
 	
-	public DataGovDownload() {
+	public DataGovDownload() throws IOException {
+		this.loginInventoryPage();
 		downloader = new URLDownload();
 	}
 	
@@ -28,13 +33,24 @@ public class DataGovDownload {
 		downloader.HTTPDownload(download_url, filename);
 	}
 	
+	// This will login into inventory page. Magic. Don't change.
+	private void loginInventoryPage() throws IOException {
+		Response document    = Jsoup.connect("https://inventory.data.gov/login_generic?came_from=/user/logged_in")
+									.data("login", "wpimhtc", "password", "wpimhtc", "remember", "63072000")
+								    .method(Method.POST)
+								    .execute();
+		cookies = document.cookies();
+	}
+	
 	// Smart file downloader, it goes into the government data API site,
 	// looks for the CSV URL of the download button, grabs and download that CSV file
 	// Tested for : https://inventory.data.gov/dataset/032e19b4-5a90-41dc-83ff-6e4cd234f565/resource/38625c3d-5388-4c16-a30f-d105432553a4
+	
 	public void smartDownload(String url, String filename) throws IOException {
 		try {
-			org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
-			String downloadLink = doc.select("a.btn.btn-primary.resource-url-analytics.resource-type-None").first().attr("href");
+			org.jsoup.nodes.Document doc = Jsoup.connect(url).cookies(cookies).get();
+			String downloadLink = doc.select("a.resource-url-analytics").first().attr("href"); 
+			System.out.println(downloadLink);
 			downloader.HTTPDownload(downloadLink, filename);
 		} catch (IOException e) {
 			e.printStackTrace();
