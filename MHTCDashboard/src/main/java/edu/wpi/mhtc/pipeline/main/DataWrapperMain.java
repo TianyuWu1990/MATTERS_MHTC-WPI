c7a154ebf3e3499e69378eb5b33067a797aa45aa
cakuhlman <cakuhlman@130.215.28.158>
2014-10-17 00:58:06 -0400

    updated data wrapper main to create data file directory if missing and to pull BLS data for all states - not just peers

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/pipeline/main/DataWrapperMain.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/pipeline/main/DataWrapperMain.java
@@ -1,5 +1,6 @@
 package edu.wpi.mhtc.pipeline.main;
 
+import java.io.File;
 import java.io.IOException;
 
 import edu.wpi.mhtc.pipeline.wrappers.BEADownload;
@@ -9,11 +10,16 @@ import edu.wpi.mhtc.pipeline.wrappers.UnZip;
 
 public class DataWrapperMain {
 
+	final static String DATA_DIRECTORY = "tmp";
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+		
+		System.out.println (new File(DATA_DIRECTORY).mkdir());
+	
+		
 		URLDownload down = new URLDownload();
 		BLSDownload bls = new BLSDownload();
 		BEADownload bea = new BEADownload();
@@ -21,38 +27,25 @@ public class DataWrapperMain {
 		
 
 		//US Census-State and Local Tax Burden
-		down.HTTPDownload("http://www2.census.gov/govs/statetax/state_tax_collections.zip", "tmp/state_tax_collections_1.zip");
-		unZip.unZipIt("tmp/state_tax_collections_1.zip","tmp");
+		down.HTTPDownload("http://www2.census.gov/govs/statetax/state_tax_collections.zip", DATA_DIRECTORY+"/state_tax_collections_1.zip");
+		unZip.unZipIt(DATA_DIRECTORY+"/state_tax_collections_1.zip", DATA_DIRECTORY);
 		
 		//NSF-TechEmployment
 //	CK-10/2014 this is original source:	
-//		down.HTTPDownload("http://www.nsf.gov/statistics/seind12/c8/tt08-54.xls", "tmp/techemployment.xls");
+//		down.HTTPDownload("http://www.nsf.gov/statistics/seind12/c8/tt08-54.xls", "DATA_DIRECTORY/techemployment.xls");
 		
 //	The nsf site has changed, and new years are available here:
-		down.HTTPDownload("http://www.nsf.gov/statistics/seind14/content/chapter-8/8-55_all.xls ", "tmp/techemployment.xls");
+		down.HTTPDownload("http://www.nsf.gov/statistics/seind14/content/chapter-8/8-55_all.xls ", DATA_DIRECTORY+"/techemployment.xls");
 		
 		//Unemployment Insurance Payroll Tax
-		down.HTTPDownload("http://www.taxpolicycenter.org/taxfacts/Content/Excel/state_unemp_rate.xls", "tmp/state_unemp_rate.xls");
+		down.HTTPDownload("http://www.taxpolicycenter.org/taxfacts/Content/Excel/state_unemp_rate.xls", DATA_DIRECTORY+"/state_unemp_rate.xls");
 		
 		//Bureau of Labor Statistics-Unemployment rate
-		bls.getBLS("ma", "2004", "2014", "tmp/");
-		bls.getBLS("ca", "2004", "2014", "tmp/");
-		bls.getBLS("co", "2004", "2014", "tmp/");
-		bls.getBLS("tx", "2004", "2014", "tmp/");
-		bls.getBLS("mn", "2004", "2014", "tmp/");
-		bls.getBLS("ga", "2004", "2014", "tmp/");
-		bls.getBLS("nc", "2004", "2014", "tmp/");
-		bls.getBLS("va", "2004", "2014", "tmp/");
-		bls.getBLS("pa", "2004", "2014", "tmp/");
-		bls.getBLS("ny", "2004", "2014", "tmp/");
-		bls.getBLS("nh", "2004", "2014", "tmp/");
-		bls.getBLS("ct", "2004", "2014", "tmp/");
-		bls.getBLS("ut", "2004", "2014", "tmp/");
-		bls.getBLS("wa", "2004", "2014", "tmp/");
-		bls.getBLS("md", "2004", "2014", "tmp/");
+		bls.getBLSAllStates("2004", "2014", DATA_DIRECTORY);
+		
 		
 		//Bureau of Economic Analysis-all regions and years
-		bea.download("http://www.bea.gov/itable/download.cfm?ext=csv&fid=B543E5DBA3B4E53FD891B19007B42B878D129B6B2748DC214E8A4E7CABFEDCCB522F771881E09A5857B449F0B6C397CB7F09B19F65C08EDE33EDFE47038C81A4", "tmp/allbea.csv");
+		bea.download("http://www.bea.gov/itable/download.cfm?ext=csv&fid=B543E5DBA3B4E53FD891B19007B42B878D129B6B2748DC214E8A4E7CABFEDCCB522F771881E09A5857B449F0B6C397CB7F09B19F65C08EDE33EDFE47038C81A4", DATA_DIRECTORY+"/allbea.csv");
 	}
 	
 	public static void twenty_source_download() throws Exception {
--- MHTCDashboard/src/main/java/edu/wpi/mhtc/pipeline/wrappers/BLSDownload.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/pipeline/wrappers/BLSDownload.java
@@ -12,17 +12,10 @@ import java.util.HashMap;
 //test push into git
 //Downloads Unemployment (Not Seasonally adjusted) Data from Bureau of Labor Statistics
 public class BLSDownload {
-
-	/**
-	 * 
-	 * @param state
-	 * @param beginyear
-	 * @param endyear
-	 * @param directory
-	 * @throws IOException
-	 */
-	public void getBLS(String state, String beginyear, String endyear, String directory) throws IOException {
-		HashMap<String, String[]> map = new HashMap<String,String[]>();
+	
+	static final HashMap<String, String[]> map;
+	static{
+		map = new HashMap<String,String[]>();
 		
 		// All States/Regions are assigned a stateID as well as a excel file name
 		map.put("AL", new String[]{"01","AL-BLS.xls"});
@@ -76,7 +69,28 @@ public class BLSDownload {
 		map.put("WV", new String[]{"49","WV-BLS.xls"});
 		map.put("WI", new String[]{"50","WI-BLS.xls"});
 		map.put("WY", new String[]{"51","WY-BLS.xls"});
+	}
 
+
+	
+	
+	
+	public void getBLSAllStates(String beginyear, String endyear, String directory) throws IOException {
+		for(String state : map.keySet()){
+			getBLS(state, beginyear, endyear, directory);
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param state
+	 * @param beginyear
+	 * @param endyear
+	 * @param directory
+	 * @throws IOException
+	 */
+	public void getBLS(String state, String beginyear, String endyear, String directory) throws IOException {
 		
 		URL url = new URL("http://data.bls.gov/pdq/SurveyOutputServlet"); // http://data.bls.gov/pdq/SurveyOutputServlet,
 																			// http://data.bls.gov/cgi-bin/dsrv
