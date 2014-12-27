cfbf8dbe22531b0d2ac747541440ad52cbeb5fde
cakuhlman <cakuhlman@130.215.28.158>
2014-12-27 17:24:44 -0500

    changed db back to dev from dev-new

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/config/DevelopmentConfiguration.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/dashboard/config/DevelopmentConfiguration.java
@@ -17,7 +17,7 @@ public class DevelopmentConfiguration {
 	
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		
-		/*dataSource.setUrl("jdbc:postgresql://mhtc.cs.wpi.edu:5432/mhtc-dev-new");
+		/*dataSource.setUrl("jdbc:postgresql://mhtc.cs.wpi.edu:5432/mhtc-dev");
 		dataSource.setUsername("server");
 		dataSource.setPassword("mhtcboxofducks");*/
 	    
