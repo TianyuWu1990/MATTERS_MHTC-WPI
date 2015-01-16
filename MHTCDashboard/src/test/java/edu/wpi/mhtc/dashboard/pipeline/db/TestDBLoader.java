75d92ae5eba75b110ab80064a84623db34b21450
cakuhlman <cakuhlman@130.215.28.158>
2015-01-16 12:32:25 -0500

    small fix to test DBLoader

--- MHTCDashboard/src/test/java/edu/wpi/mhtc/dashboard/pipeline/db/TestDBLoader.java
+++ MHTCDashboard/src/test/java/edu/wpi/mhtc/dashboard/pipeline/db/TestDBLoader.java
@@ -28,7 +28,7 @@ public class TestDBLoader {
 		assertEquals("60", map.get("total taxes"));
 		
 		map = DBLoader.getMetricInfo("2");
-		assertEquals("7", map.get("total employment"));
+		assertEquals("7", map.get("Total Employment"));
 	}
 
 }
