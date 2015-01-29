082c18229afb81058b72c0cec82c6b9113da5506
cakuhlman <cakuhlman@130.215.28.158>
2015-01-29 14:00:19 -0500

    stats service to get metrics from all subcategories

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/service/StatsServiceJDBC.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/service/StatsServiceJDBC.java
@@ -6,6 +6,7 @@ package edu.wpi.mhtc.service;
 
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
+import edu.wpi.mhtc.dashboard.pipeline.db.DBLoader;
 import edu.wpi.mhtc.model.Data.Data;
 import edu.wpi.mhtc.model.Data.DataSeries;
 import edu.wpi.mhtc.model.Data.Metric;
@@ -102,7 +104,14 @@ public class StatsServiceJDBC implements StatsService
 
 		State dbState = stateMapper.getStateFromString(state);
 		List<Metric> dbMetrics = metricsService.getMetricsInCategory(binId);
-
+		try {
+			for(String s : DBLoader.getSubCategories(binId.toString()).values()){
+				dbMetrics.addAll(metricsService.getMetricsInCategory(Integer.parseInt(s)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return getDataForState(dbState, dbMetrics);
 	}
 
