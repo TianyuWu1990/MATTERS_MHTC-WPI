31cde811843d01385d8e904c2c06568d039bd16e
Christopher Botaish <cbotaish@wpi.edu>
2015-02-18 19:43:44 -0500

    Added start of error message to be displayed when a browser is not supported.

--- MHTCDashboard/src/main/webapp/js/load.js
+++ MHTCDashboard/src/main/webapp/js/load.js
@@ -32,10 +32,33 @@ stopHeatMapAnimation=function() {
 	  	clearInterval(global_timer);
 	global_timer = null;
 };
-// new loadfunction to be called from body-onload
+
+function isCompatibleBrowser() 
+{
+	
+	
+	return true;
+}
+
+function displayBrowserVersionMsg()
+{
+}
+
+$(document).ready(function() {
+	
+	if (isCompatibleBrowser())
+	{
+		loadFunction();
+	}
+	else
+	{
+		displayBrowserVersionMsg();
+	}
+});
 
 
 loadFunction = function() {	
+	
 	cm = CM.create();
 	as = AS.create();
 	
