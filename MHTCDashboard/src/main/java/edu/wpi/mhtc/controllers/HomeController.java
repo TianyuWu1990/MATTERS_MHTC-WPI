6bcd65c86289377a7c6ef6c29eb8dd5d73cf3042
cbotaish <chris@CB-MAC.WIFI.WPI.EDU>
2015-04-24 00:32:27 -0400

    Metrics are now sorted server side before being sent to the client

--- MHTCDashboard/src/main/java/edu/wpi/mhtc/controllers/HomeController.java
+++ MHTCDashboard/src/main/java/edu/wpi/mhtc/controllers/HomeController.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -188,6 +189,20 @@ public class HomeController {
 		List<DataSeries> massTalent = statsService.getStateBinData(maId, 20);
 		List<DataSeries> massCost = statsService.getStateBinData(maId, 37);
 		List<DataSeries> massEconomy = statsService.getStateBinData(maId, 29);
+		
+		Comparator<DataSeries> dataSeriesComparator = new Comparator<DataSeries>(){
+			@Override
+			public int compare(DataSeries o1, DataSeries o2) 
+			{
+				return o1.getMetric().getDisplayOrder() - o2.getMetric().getDisplayOrder();
+			}
+		};
+		
+		massNational.sort(dataSeriesComparator);
+		massTalent.sort(dataSeriesComparator);
+		massCost.sort(dataSeriesComparator);
+		massEconomy.sort(dataSeriesComparator);
+		
 		List<State> peers = stateService.getAllPeers();
 		List<State> allstates= stateService.getAllStates(); 
 		
