92d0374b1913e033aab89010d48afa62570a4484
caitlinkuhlman <caitlinkuhlman@gmail.com>
2015-06-24 19:13:24 -0400

    Create SQL_for_Sources.sql
    Sql to set source names of metrics.

---
+++ SQL_for_Sources.sql
@@ -0,0 +1,64 @@
+update mhtc_sch.metrics set "Source" = 'NAEP (US)/TIMMS (Global)' where "Name" = '4th Grade Student Performance 
+
+Math';
+update mhtc_sch.metrics set "Source" = 'NAEP (US)/TIMMS (Global)' where "Name" = '4th Grade Student Performance 
+
+Science';
+update mhtc_sch.metrics set "Source" = 'NAEP (US)/TIMMS (Global)' where "Name" = '8th Grade Student Performance 
+
+Math';
+update mhtc_sch.metrics set "Source" = 'NAEP (US)/TIMMS (Global)' where "Name" = '8th Grade Student Performance 
+
+Science';
+update mhtc_sch.metrics set "Source" = 'US Bureau of Economic Analysis' where "Name" = 'Annual Gross State Product 
+
+in Chained $';
+update mhtc_sch.metrics set "Source" = 'US Bureau of Economic Analysis' where "Name" = 'Annual Gross State Product 
+
+Growth Rate';
+update mhtc_sch.metrics set "Source" = 'National Science Foundation' where "Name" = 'Bachelors Degree Holders in 
+
+Workforce';
+update mhtc_sch.metrics set "Source" = 'KPMG/Tax Foundation' where "Name" = 'Capital Gains Tax Rate';
+update mhtc_sch.metrics set "Source" = 'KPMG/State Tax Statutes' where "Name" = 'Corporate Income Tax Rate';
+update mhtc_sch.metrics set "Source" = 'KPMG/CCH'	 where "Name" = 'Corporate Income Tax-Apportionment 
+
+Method';
+update mhtc_sch.metrics set "Source" = 'KPMG/CCH'	 where "Name" = 'Corporate Income Tax-Sourcing Rules';
+update mhtc_sch.metrics set "Source" = 'Wanted Analytics and Monster Government Solutions' where "Name" = 'Hiring 
+
+Difficulty for Technology Industries';
+update mhtc_sch.metrics set "Source" = 'US Census Bureau' where "Name" = 'Median Household Income';
+update mhtc_sch.metrics set "Source" = 'CCH State Tax Handbook/Tax Foundation/KPMG' where "Name" = 'Personal Income 
+
+Tax';
+update mhtc_sch.metrics set "Source" = 'US Census Bureau' where "Name" = 'Quality of Life: Average Commute Time';
+update mhtc_sch.metrics set "Source" = 'E&Y' where "Name" = 'Research and Development Tax Credit ASC Rate';
+update mhtc_sch.metrics set "Source" = 'E&Y' where "Name" = 'Research and Development Tax Credit Rate';
+update mhtc_sch.metrics set "Source" = 'E&Y' where "Name" = 'Research and Development Tax Credit Refundability 
+
+Rate';
+update mhtc_sch.metrics set "Source" = 'US Energy Information Agency' where "Name" = 'Retail Price of Electricity';
+update mhtc_sch.metrics set "Source" = 'KPMG/Sales Tax Clearinghouse' where "Name" = 'Sales Tax Rate';
+update mhtc_sch.metrics set "Source" = 'KPMG/Tax Foundation' where "Name" = 'State and Local Tax Burden as Percent 
+
+of Personal Income';
+update mhtc_sch.metrics set "Source" = 'KPMG/Tax Foundation' where "Name" = 'State and Local Tax Burden Per Capita 
+
+in $';
+update mhtc_sch.metrics set "Source" = 'Tax Foundation' where "Name" = 'State Business Tax Climate Index';
+update mhtc_sch.metrics set "Source" = 'Milken Institute' where "Name" = 'State Technology and Science Index';
+update mhtc_sch.metrics set "Source" = 'National Center for Education Statistics/IPEDS' where "Name" = 'STEM-
+
+related Bachelors Degrees Awarded';                   	;
+update mhtc_sch.metrics set "Source" = 'National Science Foundation' where "Name" = 'Tech Employment as Percent of 
+
+Total Employment';
+update mhtc_sch.metrics set "Source" = 'CNBC' where "Name" = 'Top States for Business';
+update mhtc_sch.metrics set "Source" = 'National Science Foundation' where "Name" = 'Total Employment';
+update mhtc_sch.metrics set "Source" = 'National Science Foundation' where "Name" = 'Total Tech Employment';
+update mhtc_sch.metrics set "Source" = 'US Department of Labor' where "Name" = 'UI Premium Per Employee';
+
+update mhtc_sch.metrics set "Source" = 'US Bureau of Labor Statistics' where "Name" = 'Unemployment Rate';
+
+update mhtc_sch.metrics set "Source" = 'Tax Foundation'where "Name" = 'State Business Tax Climate Index';
