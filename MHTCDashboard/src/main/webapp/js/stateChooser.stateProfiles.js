d5b282555bce7a359856603d4b374b566f9ef91d
Tianyu Wu <wty773752792@gmail.com>
2015-08-03 13:12:05 -0400

    On State Profile Page, make State Chooser closed after choose any states

--- MHTCDashboard/src/main/webapp/js/stateChooser.stateProfiles.js
+++ MHTCDashboard/src/main/webapp/js/stateChooser.stateProfiles.js
@@ -267,11 +267,18 @@ $(document).ready(function() {
 				
 				$("#stateChooserTitle").click(function() {
 					$("#stateChooser").slideToggle();
+				
+				});
+				
+				$(".stateChoice").click(function() {
+					$("#stateChooser").slideUp();
+				
 				});
 				
 				$(document).click(function (e) {
 					var chooser = $("#stateChooser");
 					var title = $("#stateChooserTitle");
+					
 					
 					if (!chooser.is(e.target) && !title.is(e.target)
 							&& chooser.has(e.target).length === 0 && title.has(e.target).length === 0)
